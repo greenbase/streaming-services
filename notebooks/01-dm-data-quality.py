da5d8389d2d5c6bea8a2d691dfe13f7d111565b6
Daniel Mansfeldt <daniel.mansfeldt@fh-kiel.de>
2022-01-09 19:32:58 +0100

    docs: add comments

--- notebooks/01-dm-data-quality.py
+++ notebooks/01-dm-data-quality.py
@@ -28,13 +28,16 @@ assert all(netflix.columns==amazon.columns) & all(netflix.columns==disney.column
 # ## Check data for missing values
 # ### Missing values per feature
 
-# As the follwing dataframes show, there is a significant amount of data missing for the features "director", "country" and "date_added". Note that for the later two features the amazon data sets provides nearly no data.
+# As the follwing dataframes show, there is a significant amount of data missing for the features "director", "country" and "date_added". Note that for the later two features the amazon data set provides nearly no data.
 #
 # For none of the features containing missing values it is avisable to fill them in, since there can not be made reasonable assumptions about them. Further more they should not be removed from the data set either, because the remaining data might still be usefull for certain analytics.
 # Total amount of missing values
 #%%
+# instantiate missing value dataframes
 missing_values_total=pd.DataFrame(index=["netflix","amazon","disney","total"],columns=netflix.columns)
 missing_values_percent=missing_values_total.copy()
+
+# compute missing values (tatal,ratio) for each data set
 for service_name, service_df in zip(services.keys(),services.values()):
     titles_in_dataset_total=service_df.shape[0]
     for feature in netflix.columns: # columns are the same for all services
@@ -43,7 +46,7 @@ for service_name, service_df in zip(services.keys(),services.values()):
         missing_values_total.loc[service_name,feature]=missing_values_count
         # add ratio
         missing_values_percent.loc[service_name,feature]=missing_values_count/titles_in_dataset_total*100
-#%%
+
 # calculate total amount of missing values for all data sets and add it to the
 # corresponding dataframe
 # total
@@ -53,11 +56,12 @@ titles_total=netflix.shape[0]+amazon.shape[0]+disney.shape[0]
 missing_values_percent.loc["total"]=missing_values_total.loc["total"]/titles_total*100
 missing_values_percent=missing_values_percent.astype(float).round(2)
 
-
+#%%[markdown]
+# Total amount of missing values for each data set.
+#%%
 missing_values_total
 #%% [markdown]
 # Ration of missing values in percent compared to number of titles in each dataset.
-
 #%%
 missing_values_percent
 # %%
